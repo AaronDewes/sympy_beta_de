fdcf5ce4c3c9871bae5e01c83d23da8bcc61de0a
Aaron Dewes <aaron@runcitadel.space>
2023-05-08 07:16:28 +0000

    Fix file name

---
+++ scripts/build_antlr.mjs
@@ -0,0 +1,14 @@
+import { spawnSync } from 'child_process'
+import { renameSync } from 'fs'
+import { chdir } from 'process'
+import { SOURCE_DATE_EPOCH, ensure, encoding, python } from './util.mjs'
+
+const version = '4.12'
+
+console.log('Start build_antlr.mjs.')
+ensure(spawnSync(python, ['-m', 'pip', 'download', `antlr4-python3-runtime==${version}`], { encoding }), 'Fail to download antlr4.')
+
+const wheel = `antlr4_python3_runtime-${version}-py3-none-any.whl`
+renameSync(wheel, `../public/${wheel}`)
+
+console.log('Finish build_antlr.mjs.')
